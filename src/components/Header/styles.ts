ab4bbab7974b78fbbbc5f06b01032d21855c843f
Carlos Pereira <ce.magalhaespereira@gmail.com>
2023-02-28 18:45:38 +0000

    Add Header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,19 @@
+import { clamp } from 'lodash'
+import styled from 'styled-components'
+
+interface Props {
+  mode: string
+}
+
+export const Container = styled.header<Props>`
+  .type {
+    text-transform: uppercase;
+    font-size: 0.9rem;
+  }
+
+  .title {
+    color: ${(props) =>
+      (props.mode === 'red' && '#ED1C24') ||
+      (props.mode === 'green' && '#1F96A2')};
+  }
+`
