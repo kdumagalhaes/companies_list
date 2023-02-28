95251c6579dd13cf8fd0afef28b6a61d63261cde
Carlos Pereira <ce.magalhaespereira@gmail.com>
2023-02-28 20:26:14 +0000

    Add Home page

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,35 +1,21 @@
 import { Container } from './styles'
 import { Modes } from '../../styles/themes/default'
-
+import { ReactNode } from 'react'
 interface ListProps {
   mode: Modes
   title1: string
   title2: string
+  children: ReactNode
 }
 
-export function List({ mode, title1, title2 }: ListProps) {
+export function List({ mode, title1, title2, children }: ListProps) {
   return (
     <Container mode={mode}>
       <ul className="title">
         <li className="title-1">{title1}</li>
         <li className="title-2">{title2}</li>
       </ul>
-      <ul className="data-row">
-        <li className="data-row-1">
-          <a href="#" className="data-link">
-            Local Public Office
-          </a>
-        </li>
-        <li className="data-row-2">500754012</li>
-      </ul>
-      <ul className="data-row">
-        <li className="data-row-1">
-          <a href="#" className="data-link">
-            Local Office
-          </a>
-        </li>
-        <li className="data-row-2">500754012</li>
-      </ul>
+      {children}
     </Container>
   )
 }
