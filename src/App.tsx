fbe6fef53b339c771d38f9922eb66b2937ba6770
Carlos Pereira <ce.magalhaespereira@gmail.com>
2023-02-28 20:35:03 +0000

    Add Router configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
-import { Home } from './pages/Home/Home'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './routes/Router'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Home />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
---
+++ src/routes/Router.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from 'react-router-dom'
+import { Home } from '../pages/Home/Home'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/company/:companyId" />
+    </Routes>
+  )
+}
