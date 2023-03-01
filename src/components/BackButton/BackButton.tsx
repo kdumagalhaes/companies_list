63c9c4ae0e896f733edf6015092eb89cbbce1cd9
Carlos Pereira <ce.magalhaespereira@gmail.com>
2023-03-01 18:12:51 +0000

    Add back icon && improve pages style

--- src/components/BackButton/BackButton.tsx
+++ src/components/BackButton/BackButton.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 import { Button } from './styles'
+import BackIcon from '../../assets/icons/back-arrow.svg'
 
 export function BackButton() {
   const navigate = useNavigate()
@@ -8,6 +9,7 @@ export function BackButton() {
   }
   return (
     <Button data-testid="back-button-test" onClick={goBack}>
+      <img src={BackIcon} className="back-arrow" alt="back arrow" />
       Back
     </Button>
   )
